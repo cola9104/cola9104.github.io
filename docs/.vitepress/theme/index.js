db4f0afaa53d972b61d9967363654669c74583d5
Cola <1064522588@qq.com>
2025-11-20 14:21:17 +0800

    修复页面显示为白色的问题：移除错误CSS引用、优化路由监听逻辑、增强数据加载机制

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,3 +1,4 @@
+import DefaultTheme from 'vitepress/theme'
 import './blog.css'
 
 // 导入客户端配置
@@ -6,15 +7,19 @@ import enhanceClient from '../client.js'
 // 导入Notion插件
 import { NotionPlugin } from '../plugins/notionPlugin.js'
 
+// 导入Vue组件
+import BlogWithApi from '../components/BlogWithApi.vue'
+
 export default {
+  extends: DefaultTheme,
   enhanceApp({ app, router }) {
     // 注册Notion插件
     app.use(NotionPlugin)
-    
+
     // 使用客户端增强函数，并传入router
     enhanceClient(app, router)
-    
+
     // 注册全局组件
-    // 可以在这里注册其他全局组件
+    app.component('BlogWithApi', BlogWithApi)
   }
 }
